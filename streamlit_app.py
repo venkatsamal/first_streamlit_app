fdbfe502b6ac4bcf88a9d192493402415eda21fb
venkatsamal <148716966+venkatsamal@users.noreply.github.com>
2023-10-23 00:53:49 +0530

    Update streamlit_app.py
    ne

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,8 +9,9 @@ streamlit.text('3')
 import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
+
 # Let's put a pick list 
-streamlit.multiselect("Pick some fruits - ", list(my_fruit_list.index))
+streamlit.multiselect("Pick some fruits - ", list(my_fruit_list.index), ['Avocado', 'Strawberries'])
 
 #display the table on page
 streamlit.dataframe(my_fruit_list)
