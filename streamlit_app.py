84a80c5ca8354d1c40822e8e787232530ee7421e
venkatsamal <148716966+venkatsamal@users.noreply.github.com>
2023-10-23 00:42:53 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,3 @@
-import streamlit
-
-streamlit.title('My parents new healthy diner')
-streamlit.header('Breakfast Menu')
-streamlit.text('1')
-streamlit.text('2')
-streamlit.text('3')
 
 import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
