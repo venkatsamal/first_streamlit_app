22452dce7d5b2251cd410d0bfa348ec72dcbf7cb
venkatsamal <148716966+venkatsamal@users.noreply.github.com>
2023-10-23 19:58:50 +0530

    Update snowflake_app.py

--- snowflake_app.py
+++ snowflake_app.py
@@ -1,6 +1,13 @@
-'Hi'
+import streamlit
 import pandas
 import snowflake.connector
+
+streamlit.title('My parents new healthy diner')
+streamlit.header('Breakfast Menu')
+streamlit.text('1')
+streamlit.text('2')
+streamlit.text('3')
+
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
