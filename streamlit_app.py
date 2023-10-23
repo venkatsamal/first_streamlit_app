c59cd0d8bbb4d542dc0c04ed79bc53b1bb1b867d
venkatsamal <148716966+venkatsamal@users.noreply.github.com>
2023-10-23 19:40:35 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,14 +6,6 @@ streamlit.text('1')
 streamlit.text('2')
 streamlit.text('3')
 
-import pandas
-my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-my_fruit_list = my_fruit_list.set_index('Fruit')
-
-# Let's put a pick list 
-fruits_selected = streamlit.multiselect("Pick some fruits - ", list(my_fruit_list.index), ['Avocado', 'Strawberries'])
-fruits_to_show = my_fruit_list.loc[fruits_selected]
-
 
 import snowflake.connector
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
