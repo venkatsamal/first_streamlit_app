435d715b09979a187feed0f799fda0cd2d5a38d0
venkatsamal <148716966+venkatsamal@users.noreply.github.com>
2023-10-23 20:31:20 +0530

    Update snowflake_app.py

--- snowflake_app.py
+++ snowflake_app.py
@@ -1,17 +1,24 @@
 import streamlit
 import pandas
+import requests
 import snowflake.connector
-
+from urllib.error import URLError
 streamlit.title('My parents new healthy diner')
 streamlit.header('Breakfast Menu')
 streamlit.text('1')
 streamlit.text('2')
 streamlit.text('3')
 
+streamlit.header("Fruityvice Fruit choice")
+fruit_choice = streamlit.text_input('What fruit')
+streamlit.write('user entered', fruit_choice) 
+streamlit.stop()
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
-my_cur.execute("insert into fruit_load_list values ('From Streamlit')")
-my_data_row = my_cur.fetchone()
-streamlit.text("Fruit List from Snowflake:")
-streamlit.text(my_data_row)
+my_cur.execute("select * from fruit_load_list values")
+my_data_row = my_cur.fetchall()
+streamlit.header("Fruit List from Snowflake:")
+streamlit.dataframe(my_data_row)
+
+
